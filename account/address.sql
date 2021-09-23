56842b5a707530ae6e78e825522ae5123ebcf840
lindsaygelle <lindsay.gelle@gmail.com>
2021-09-23 11:26:12 +1000

    Added to ./account

--- account/address.sql
+++ account/address.sql
@@ -2,11 +2,11 @@ CREATE TABLE IF NOT EXISTS account.address
 (
     name CITEXT NOT NULL,
     profile_id INT NOT NULL,
-    time_created TIMESTAMPTZ NOT NULL,
-        DEFAULT CURRENT_TIMESTAMP,
-    time_edited TIMESTAMPTZ NOT NULL
-        DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (name),
+    FOREIGN KEY (name)
+        REFERENCES account.profile (name)
+        MATCH SIMPLE
+        ON DELETE CASCADE,
     FOREIGN KEY (profile_id)
         REFERENCES account.profile (id)
         MATCH SIMPLE
