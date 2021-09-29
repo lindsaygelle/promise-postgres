ba6d54738ef1a84746e659c2283443790cc24d47
lindsaygelle <lindsay.gelle@gmail.com>
2021-09-29 13:27:29 +1000

    Added to ./email

--- email/verification.sql
+++ email/verification.sql
@@ -1,18 +1,18 @@
 CREATE TABLE IF NOT EXISTS email.verification
 (
-    address_id INT NOT NULL,
     code UUID NOT NULL
         DEFAULT gen_random_uuid(),
+    profile_id INT NOT NULL,
     time_created TIMESTAMP NOT NULL
         DEFAULT CURRENT_TIMESTAMP,
     time_due TIMESTAMP NOT NULL
         DEFAULT CURRENT_TIMESTAMP + INTERVAL '1' DAY,
-    PRIMARY KEY (address_id),
-    FOREIGN KEY (address_id) 
-        REFERENCES email.address (id)
-        MATCH SIMPLE
+    PRIMARY KEY (profile_id),
+    FOREIGN KEY (profile_id) 
+        REFERENCES email.address (profile_id) MATCH FULL
         ON DELETE CASCADE
         ON UPDATE NO ACTION
+        NOT VALID
 );
 
 ALTER TABLE email.verification
